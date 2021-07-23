9cbc9ac3f582eacbb44ee7bff348204664cd3938
willjw3 <will.j.ward3@gmail.com>
2021-07-23 18:59:53 +0900

    Removed unnecessary comments

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
 
-//let url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 
 
